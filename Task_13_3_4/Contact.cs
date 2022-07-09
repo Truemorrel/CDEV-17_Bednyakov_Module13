b50e6ca6485be913e7c5f88b31c41be4e2d5e0c8
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-09 20:39:29 +0300

    master: add tasks 13.3.10 -10.3.12

--- Task_13_3_4/Contact.cs
+++ Task_13_3_4/Contact.cs
@@ -1,59 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace PhoneBook
 {
-    class Program
+    public class Contact
     {
-        //  Объявим словарь с двумя значениями
-        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
+        public String Name { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+
+        public Contact(string name, long phoneNumber, String email) // метод-конструктор
         {
-            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
-            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
-        };
-
-        static void Main(string[] args)
-        {
-            // Покажем весь список
-            Console.WriteLine("Текущий список контактов: ");
-            WriteAllContacts();
-
-            // Попробуем добавить новый контакт, если такого ещё нет
-            PhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
-
-            //  Выведем обновлённый список
-            Console.WriteLine("Обновленный список контактов: ");
-            WriteAllContacts();
-
-            //  Попробуем достать контакт для изменения данных
-            if (PhoneBook.TryGetValue("Диана", out Contact contact))
-                contact.PhoneNumber = 79990000001;
-
-            // И покажем результат после изменения
-            Console.WriteLine("Список после изменения: ");
-            WriteAllContacts();
-        }
-
-        // Метод для вывода словаря на консоль
-        public static void WriteAllContacts()
-        {
-            foreach (var contact in PhoneBook)
-                Console.WriteLine(contact.Key + ": " + contact.Value.PhoneNumber);
-            Console.WriteLine();
-        }
-
-        // Класс был изменен. Поле Name мы убрали,
-        // так как оно теперь будет ключом
-        // для поиска значений в словаре
-        public class Contact // модель класса
-        {
-            public Contact(long phoneNumber, String email) // метод-конструктор
-            {
-                PhoneNumber = phoneNumber;
-                Email = email;
-            }
-            public long PhoneNumber { get; set; }
-            public String Email { get; set; }
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
         }
     }
 }
--- Task_13_4_4/Program.cs
+++ Task_13_4_4/Program.cs
@@ -1,44 +1,59 @@
 ﻿using System;
-using System.Collections.Generic; // подключаем пространство имён с необобщёнными коллекциями
+using System.Collections.Generic;
 
-namespace Task_13_4_4
+namespace PhoneBook
 {
     class Program
     {
+        //  Объявим словарь с двумя значениями
+        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
+        {
+            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
+            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
+        };
+
         static void Main(string[] args)
         {
-            //  создаём пустой список с типом данных Contact
-            var phoneBook = new Phonebook ()
-            {
-                ["Игорь"].Add(new Contact { 79990000000, "igor@example.com" }),
-                ["Андрей"] = new Contact { 79990000001, "andrew@example.com" },
-            };
+            // Покажем весь список
+            Console.WriteLine("Текущий список контактов: ");
+            WriteAllContacts();
 
-            // проверяем, что добавилось в список
-            foreach (var contact in phoneBook)
-            {
-                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
-            }
-            AddUniq(men, phoneBook);
+            // Попробуем добавить новый контакт, если такого ещё нет
+            PhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
+
+            //  Выведем обновлённый список
+            Console.WriteLine("Обновленный список контактов: ");
+            WriteAllContacts();
+
+            //  Попробуем достать контакт для изменения данных
+            if (PhoneBook.TryGetValue("Диана", out Contact contact))
+                contact.PhoneNumber = 79990000001;
+
+            // И покажем результат после изменения
+            Console.WriteLine("Список после изменения: ");
+            WriteAllContacts();
         }
-        static void AddUniq(Contact contact, Phonebook phoneBook)
+
+        // Метод для вывода словаря на консоль
+        public static void WriteAllContacts()
         {
-            phoneBook.Add(contact);
-            var isInList = false;
-            foreach (var record in phoneBook)
+            foreach (var contact in PhoneBook)
+                Console.WriteLine(contact.Key + ": " + contact.Value.PhoneNumber);
+            Console.WriteLine();
+        }
+
+        // Класс был изменен. Поле Name мы убрали,
+        // так как оно теперь будет ключом
+        // для поиска значений в словаре
+        public class Contact // модель класса
+        {
+            public Contact(long phoneNumber, String email) // метод-конструктор
             {
-                isInList = (contact == record) ? contact == record : isInList;
+                PhoneNumber = phoneNumber;
+                Email = email;
             }
-            if (!isInList)
-            {
-                phoneBook.Add(contact);
-            }
-            phoneBook.Sort((x, y) => string.Compare(x.Name, y.Name));
-            foreach (var record in phoneBook)
-            {
-                Console.WriteLine($"{record.Name}: {record.PhoneNumber}");
-            }
+            public long PhoneNumber { get; set; }
+            public String Email { get; set; }
         }
     }
-
 }
